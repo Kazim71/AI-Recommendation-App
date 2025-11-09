653a42daeadbf5ea8d898859b811a645077f0a11
Mohammad Kazim <118991770+Kazim71@users.noreply.github.com>
2025-11-10 01:46:08 +0530

    Update

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // -------------------- CORS Helper --------------------
 const allowedOrigins = [
-  "https://ai-recommendation-app-zhyc.vercel.app",
+  "https://ai-recommendation-app-ex8f.vercel.app/",
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
